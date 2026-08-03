5000f245e2ae4b3fac47297975a5f83658f0d9e7
gnouy657-cell <gnouy657@gmail.com>
2026-08-02 22:32:10 -0400

    added JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem-container", {
+    strings: "Generating poem...",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
