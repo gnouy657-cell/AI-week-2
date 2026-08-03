2d28a546bbbbb015c76ebe05b7a3b6f7abaa06dd
gnouy657-cell <gnouy657@gmail.com>
2026-08-03 08:17:13 -0400

    fixed js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function generatePoem(event) {
   new Typewriter("#poem-container", {
     strings: "Generating poem...",
     autoStart: true,
-    delay: 1,
+    delay: 5,
     cursor: "",
   });
 }
