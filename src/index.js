96a1f3784e7a3d3d9620c82f994302365d30dff4
gnouy657-cell <gnouy657@gmail.com>
2026-08-04 07:23:53 -0400

    added animation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 function displayPoem(response) {
   console.log("poem generated");
+
+  let poemContainer = document.querySelector("#poem-container");
+
+  poemContainer.innerHTML = "";
+
   new Typewriter("#poem-container", {
     strings: response.data.answer,
     autoStart: true,
@@ -12,10 +17,26 @@ function generatePoem(event) {
   event.preventDefault();
 
   let topicInput = document.querySelector("#user-input");
+  let poemContainer = document.querySelector("#poem-container");
+
+  poemContainer.innerHTML = `
+    <p>Generating poem about <strong>${topicInput.value}</strong>...</p>
+    <div class="bounce">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  `;
+
+  poemContainer.classList.remove("hidden");
+
   let apiKey = "eob2a41574f3at947904539fe34b012a";
-  let prompt = `Generate a poem about ${topicInput.value}`;
+
+  let prompt = `Generate a poem about ${encodeURIComponent(topicInput.value)}`;
+
   let context =
-    "You are a poet. Write a creative, elegant poem about the user's topic. Use simple language, vivid imagery, and metaphors. Make it exactly 8 lines. Generate an original poem in basic HTML only. Use <p> elements for each line. Do NOT wrap the response in Markdown code fences. Do NOT include ```html or ``` anywhere. Return only the HTML. Credit SheCodes AI at the end of the poem wrapping it in <strong> inclduing a dash before SheCodes AI.";
+    "You are a poet. Write a creative, elegant poem about the user's topic. Use simple language, vivid imagery, and metaphors. Make it exactly 8 lines. Generate an original poem in basic HTML only. Use <p> elements for each line. Do NOT wrap the response in Markdown code fences. Do NOT include ```html or ``` anywhere. Return only the HTML. Credit SheCodes AI at the end of the poem wrapping it in <strong> including a dash before SheCodes AI.";
+
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating poem...");
@@ -26,4 +47,5 @@ function generatePoem(event) {
 }
 
 let poemFormElement = document.querySelector("#poem-form");
+
 poemFormElement.addEventListener("submit", generatePoem);
